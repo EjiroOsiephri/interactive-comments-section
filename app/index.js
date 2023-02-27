6c2dc50d5d501b1f4b547dcd825c1b5d7d7e48e7
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-02-27 23:13:23 +0100

    responsiveness added

---
+++ app/index.js
@@ -0,0 +1,11 @@
+async function getData() {
+  try {
+    const response = await fetch("data.json");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getData();
